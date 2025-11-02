b879500645e3ec3f860d93b154c57764e3bd2788
patryk-polanski <pat.polanski@gmail.com>
2025-11-03 00:34:16 +0100

    create key.pem and cert.pem files, modify server to use https connection

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
 const express = require("express");
 
@@ -9,6 +11,18 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+app.get("/secret", (req, res) => {
+  return res.send("Your personal secret value is 42!");
 });
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
